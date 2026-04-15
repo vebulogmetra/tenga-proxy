2d9b10c6518f21980918aca1bd2fc1338e9fdd84
Artem G. <vebulogmetra@yandex.ru>
2026-04-15 11:26:59 +0300

    version 0.10.3

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.2"
+__version__ = "0.10.3"
 __app_name__ = "Tenga Proxy"
 __app_description__ = (
     "Клиент прокси с бекэндом <a href='https://github.com/XTLS/Xray-core'>xray-core</a>"
